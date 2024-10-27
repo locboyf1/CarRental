ea67c07baa1f30e2f6548eb1ab31471b92b62fdd
locboyf1 <locboyf1@gmail.com>
2024-10-28 00:35:01 +0700

    Nhận biết sai trong chức năng đăng nhập

--- Program.cs
+++ Program.cs
@@ -25,4 +25,9 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+app.MapControllerRoute(
+    name: "areas",
+    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+);
+
 app.Run();
